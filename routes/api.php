51addfad6600e09bfc58c41f24985fddbf68f66a
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-03-05 03:16:53 +0000

    added auth routes and controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', [\App\Http\Controllers\Api\AuthController::class, 'user']);
+});
