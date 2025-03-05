68295941f085eefe211b0925f391cac687978772
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-03-05 04:20:56 +0000

    updated AboutView.vue

--- resources/js/views/AboutView.vue
+++ resources/js/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="max-w-7xl mx-auto px-4">
+      <h1 class="text-center text-4xl font-bold mt-16">
+          Todo list App
+      </h1>
+
+        <p class="text-center mt-4">
+            This is a simple todo list app built with Vue 3, Laravel 12, Sanctum and Tailwind CSS
+        </p>
   </div>
 </template>
